0e29647efc4714a07a4a6f5e97d0b4bfdbc01e3a
cjbech01 <cjbech01@cardmail.louisville.edu>
2017-11-22 19:22:51 -0500

    Add files via upload

---
+++ DatabaseConnection.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function Connect()
+{
+ $dbhost = "auctionit.fbcmtown.org";
+ $dbuser = "fbcmtown_auction";
+ $dbpass = "";
+ $dbname = "fbcmtown_auctionITdb";
+
+ // Create connection
+ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
+
+ return $conn;
+}
+ 
