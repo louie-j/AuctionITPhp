63dd35f09a0610f6b7df428a8a5bc8951cce6014
therealmuska <dantheman.prez@gmail.com>
2018-04-04 10:48:24 -0400

    No changes

--- AdminPage.php
+++ AdminPage.php
@@ -30,8 +30,10 @@ and open the template in the editor.
                     "columns": [
                         { mData: 'Username', "searchable": true } ,
                         { mData: 'Type', "searchable": false },
-                        { mData: 'Active', "searchable": false }
-                         ] 
+                        { mData: 'Active', "searchable": false },
+                        {  "targets": -1,
+                            "data": null,
+                            "defaultContent": "<button>Edit</button>"} ] 
                 } );
 
                 //Function that responds to user clicking edit button
@@ -63,26 +65,27 @@ and open the template in the editor.
 
             function changePagesAutomatically()
             {
-                var table = $('#myDataTable').DataTable();
-                if(interval)
-            $( document ).ready(function() {
-                if(<?php echo $_SESSION['databaseSuccess'] ?> === 1)
-                {
-                    alert("User created");
-                    <?php $_SESSION['databaseSuccess'] = 0 ?>
-                }
-                else if(<?php echo $_SESSION['databaseSuccess'] ?> === 2)
-                {
-                    clearInterval(interval);
-                    interval = null;
-                }
-                else
-                {
-                    interval = setInterval( function () {
-                        var info = table.page.info();
-                        var pageNum = (info.page < info.pages) ? info.page + 1 : 1;
-                        table.page(pageNum).draw(false); 
-                    }, 10000);                   
+                    var table = $('#myDataTable').DataTable();
+                    if(interval)
+                $( document ).ready(function() {
+                    if(<?php echo $_SESSION['databaseSuccess'] ?> === 1)
+                    {
+                        alert("User created");
+                        <?php $_SESSION['databaseSuccess'] = 0 ?>
+                    }
+                    else if(<?php echo $_SESSION['databaseSuccess'] ?> === 2)
+                    {
+                        clearInterval(interval);
+                        interval = null;
+                    }
+                    else
+                    {
+                        interval = setInterval( function () {
+                            var info = table.page.info();
+                            var pageNum = (info.page < info.pages) ? info.page + 1 : 1;
+                            table.page(pageNum).draw(false); 
+                        }, 10000);                   
+                    }
                 }
             }
 		
