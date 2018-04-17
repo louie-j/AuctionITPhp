e66cf01ac62322cb143fc28efbe6452debbdde7e
Ewen <ecameron@interapt.com>
2018-04-17 17:57:46 -0400

    Need to make local db, switch

--- PhpScripts/DatabaseConnection.php
+++ PhpScripts/DatabaseConnection.php
@@ -14,17 +14,17 @@ function Connect()
     $dbpass = "";
     $dbname = "fbcmtown_auctionITdb"; */
 
-    /* Master
+    /* Master*/
     $dbhost = "auctionit.fbcmtown.org";
     $dbuser = "fbcmtown_auction";
     $dbpass = "";
-    $dbname = "fbcmtown_auctionITdb"; */
+    $dbname = "fbcmtown_auctionITdb"; 
     
-    /* Tyler */
+    /* Tyler *
     $dbhost = "localhost";
     $dbuser = "root";
     $dbpass = "";
-    $dbname = "AuctionIT";
+    $dbname = "AuctionIT";*/
 
     // Create connection
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
