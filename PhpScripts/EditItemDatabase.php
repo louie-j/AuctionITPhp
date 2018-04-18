00d43333d36f2ceb83e1b043f1851b8f607192cd
ebmcla01 <ebmcla01@louisville.edu>
2018-04-17 20:40:21 -0400

    Allow items to have "priceless" value

--- PhpScripts/EditItemDatabase.php
+++ PhpScripts/EditItemDatabase.php
@@ -5,7 +5,7 @@
    $auctionId = strip_tags($_POST['auctionId']) == null ? 'null' : strip_tags($_POST['auctionId']);
    $description = strip_tags($_POST['description']) == null ? 'null' : strip_tags($_POST['description']);
    $donatedBy = strip_tags($_POST['donatedBy']) == null ? 'null' : strip_tags($_POST['donatedBy']);
-   $value = strip_tags($_POST['value']) == null ? 'null' : strip_tags($_POST['value']);
+   $value = strip_tags($_POST['value']) == null ? -1 : strip_tags($_POST['value']);
    $userId = $_SESSION['autoID'];
 
    $conn = Connect();
