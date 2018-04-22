a3e1683afcaa65255e2a78cf5407ebc3c34005af
ebmcla01 <ebmcla01@louisville.edu>
2018-04-22 12:38:39 -0400

    allow phone number to be optional for bidders

--- PhpScripts/EditBidder.php
+++ PhpScripts/EditBidder.php
@@ -2,13 +2,18 @@
    session_start();
    require 'DatabaseConnection.php';
    $bidderId = strip_tags($_POST['bidderId']);
-   $phone = strip_tags($_POST['phone']) == null ? 'null' : "'" . strip_tags($_POST['phone']) . "'";
-   $address = strip_tags($_POST['address']) == null ? 'null' : "'" . addslashes(strip_tags($_POST['address'])) . "'";
-   $name = strip_tags($_POST['name']) == null ? 'null' : "'" . addslashes(strip_tags($_POST['name'])) . "'";
-   $phone = preg_replace("/\D/", "", $phone);
+   $phone = (strip_tags($_POST['phone']) == null || empty(strip_tags($_POST['phone'])))
+            ? 'null' 
+            : "'" . preg_replace("/\D/", "", strip_tags($_POST['phone'])) . "'";
+   $address = (strip_tags($_POST['address']) == null || strip_tags($_POST['address']) == "")
+            ? 'null' 
+            : "'" . addslashes(strip_tags($_POST['address'])) . "'";
+   $name = (strip_tags($_POST['name']) == null || strip_tags($_POST['name']) == "")
+            ? 'null' 
+            : "'" . addslashes(strip_tags($_POST['name'])) . "'";
    $conn = Connect();
    $sql = "CALL updateBidder ('" . $bidderId . "'," . $name . "," . $address . "," . $phone . ")";
-   
+  
    $result = $conn->query($sql);
 
    if (!$result) {
